dd8a37b4dd04a5c18ebce4cbf46a6799a198b6db
Jason Farkas <jasonrfarkas@gmail.com>
2016-05-10 11:00:41 -0400

    Setup(gamelogic.java): Created inital frame based of class diagram with 3 comments

---
+++ GameLogic.java
@@ -0,0 +1,132 @@
+public class GameLogic{
+	private int currentRound
+	private int currentSquare
+	private Char gameBoard[]
+	private int scoreBoard[2]
+	private int SecretSquare
+	private boolean PickedSecretSquare
+	private String PrizesGiven[]
+	private boolean CurrentPlayer
+	private QuestionBank myQuestionBank
+	private TicTacToeGui Display
+	private boolean Multiplayer
+	private boolean CorrectAnswer
+	private int roundsWonP1
+	private int roundsWonP2
+	private TimerObject Timer
+
+	public GameLogic(){
+
+	}
+
+	public setMultiplayer(boolean m){
+
+	}
+
+	// 99% SURE THIS MUST BE CHANGED TO PUBLIC TO WORK
+	private StartGame(){
+
+	}
+
+	private StartRounds(){
+
+	}
+
+	private UpdateRounds(){
+
+	}
+
+	private SecretSquareSetup(){
+
+	}
+	// THIS IS CLEARLY A PROBLEM SINCE IT IS RETURN TYPE VOID FOR A GETTER
+	public GetQuesiton(){
+
+	}
+
+	public int GetRoundNumber(){
+
+	}
+
+	public String getShape(){
+
+	}
+	// THIS IS CLEARLY A PROBLEM SINCE IT IS RETURN TYPE VOID FOR A GETTER
+	public getPrize(){
+
+	}
+
+	private SwitchPlayer(Player: boolean){
+
+	}
+
+	private PlayerTurnStart(){
+
+	}
+
+	private StartTimer(){
+
+	}
+
+	private TimeUp(){
+
+	}
+
+	private KillTimer(){
+
+	}
+
+	private ContinueTurn(){
+
+	}
+
+	public boolean PickSquare(int square){
+
+	}
+
+	private CheckSquareEmpty(int square){
+
+	}
+
+	private UpdateScoreBoard(){
+
+	}
+
+	private int RoundWonCheck(){
+
+	}
+
+	private boolean AnswerQuestion(Boolean a){
+
+	}
+
+	private SetSquare(char c){
+
+	}
+
+	private UpdateGameboard(int CurrentSquare){
+
+	}
+
+	private UpdateBoardBasedOnAnswer(){
+
+	}
+
+	private endTurn(){
+
+	}
+
+	private int checkWinner(){
+
+	}
+
+	private int GameWonCheck(int i){
+
+	}
+
+	
+
+
+
+
+}// end GameLogic
