842c09f7d79823e74ebe712627b23a0345ae2400
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2016-05-10 17:53:31 -0400

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -83,18 +83,35 @@ public class GameLogic{
 	public boolean PickSquare(int square){
 
 	}
-
-	private CheckSquareEmpty(int square){
-
-	}
+	
+	private boolean CheckSquareEmpty(int square){
+	if(gameBoard[square]=='x'||gameBoard[square]=='o'){
+		return false;
+	}//if
+	else{
+		return true;
+	}//else
+	}//CheckSquareEmpty
 
 	private UpdateScoreBoard(){
 
 	}
 
 	private int RoundWonCheck(){
-
-	}
+	if(gameBoard[0]==gameBoard[1]==gameBoard[2]||	//across top
+	   gameBoard[3]==gameBoard[4]==gameBoard[5]||	//across middle
+	   gameBoard[6]==gameBoard[7]==gameBoard[8]||	//across bottom
+	   gameBoard[0]==gameBoard[3]==gameBoard[6]||	//down left
+	   gameBoard[1]==gameBoard[4]==gameBoard[7]||	//down middle
+	   gameBoard[2]==gameBoard[5]==gameBoard[8]||	//down right
+	   gameBoard[0]==gameBoard[4]==gameBoard[8]||	//diagonal L>R
+	   gameBoard[2]==gameBoard[4]==gameBoard[6]){	//diagonal R>L
+	   	return true;
+	   }//if
+	   else{
+	   	return false;
+	   }//else
+	}//RoundWonCheck
 
 	private boolean AnswerQuestion(Boolean a){
 
@@ -117,11 +134,19 @@ public class GameLogic{
 	}
 
 	private int checkWinner(){
-
+	
 	}
-
+	//not sure what int i is but id assume this is how its supposed to look- Tyler
 	private int GameWonCheck(int i){
-
+	if(roundsWonP1==2){
+		return 1;
+	}//if
+	else if (roundsWonP1==2){
+		return 1;
+	}//else if
+	else{
+		return -1;
+	}//else
 	}
 
 	
