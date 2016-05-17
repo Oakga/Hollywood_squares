56437200c78968100e34f0055b5afdbe3b271ad3
Oakga <oakioakga@gmail.com>
2016-05-17 11:40:17 -0400

    Added a main function

---
+++ Game.java
@@ -0,0 +1,12 @@
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TicTacToeGui GUI = null;
+		GameLogic game=new GameLogic(GUI);
+		GUI=new TicTacToeGui(game);
+		game.StartGame();
+	}
+
+}
