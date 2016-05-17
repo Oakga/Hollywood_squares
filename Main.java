112a0c4b39cda4e7b96a35a93040749ff1892be1
Jason Farkas <jasonrfarkas@gmail.com>
2016-05-17 15:57:16 -0400

    Fixed mistate from last merge in main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TicTacToeGui GUI = null;
-		GameLogic game=new GameLogic(GUI);
-		GUI=new TicTacToeGui(game);
-		game.StartGame();
-=======
 public class Main{
+
   	public static void main(String[] args)
 	{
-	GameLogic y = new GameLogic();
-	TicTacToeGui x = new TicTacToeGui(y);
-	x.setVisible(true);
->>>>>>> 2771762049edde73d3ea7ecd9e0761ccab76d406
+		GameLogic game = new GameLogic();
+		game.StartGame();
 	}
 }
